cebf8eae70af4699af8832b6a2f56e1540451fcc
jpisani0 <jgp9201@gmail.com>
2024-09-17 22:24:05 -0400

    moved funcs for generating password to generatePassword.py

--- main.py
+++ main.py
@@ -4,44 +4,13 @@
 #
 # DESCRIPTION: main file for passgen
 
-import random
 import sys
 
-from options import *
 from userInput import getOptions
-
-
-# Generate a random character for the password based on user settings
-def genCharacter():
-    character = ""
-
-    while character == "":
-        character = chr(random.randint(33, 126))
-
-        # If symbols are disabled
-        if not symbols:
-            if checkSymbol(character):
-                character = ""
-
-        # If numbers are disabled
-        if not numbers:
-            if checkNumber(character):
-                character = ""
-
-    return character
-
-
-# Generate the random password
-def genPasswd():
-    passwd = ""
-
-    while len(passwd) < length:
-        passwd = passwd + genCharacter()
-
-    return passwd
+from generatePassword import genPasswd
 
 
 length, symbols, numbers = getOptions(sys.argv)
 
-print("WARNING: your CLI has likely logged this password in plain text on your machine. Consider manually deleting it from the log for maximum security.")
-print(genPasswd())
+print("passgen(): WARNING: your CLI has likely logged this password in plain text on your machine. Consider manually deleting it from the log for maximum security.")
+print(genPasswd(length, symbols, numbers))
